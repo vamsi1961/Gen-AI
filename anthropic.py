323347e3b953c96cf207359c4921d73fba02b249
vamsi1961 <cheedellavamsikishore@gmail.com>
2025-03-29 12:43:11 +0530

    anthropic, router agent

---
+++ anthropic.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+load_dotenv()
+
+
+@tool
+def multiply(x: float, y: float) -> float:
+    """Multiply 'x' times 'y'."""
+    return x * y
+
+
+if __name__ == "__main__":
+    print("Hello Tool Calling")
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "you're a helpful assistant"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
+
+    tools = [TavilySearchResults(), multiply]
+    # llm = ChatOpenAI(model="gpt-4-turbo")
+    llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=0)
+
+    agent = create_tool_calling_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+    res = agent_executor.invoke(
+        {
+            "input": "what is the weather in dubai right now? compare it with San Fransisco, output should in in celsious",
+        }
+    )
+
+    print(res)
---
+++ code-interpreter/csv_agent.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+
+
+load_dotenv()
+
+
+def main():
+    print("Start...")
+
+
+    csv_agent = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4o"),
+        path="episode_info.csv",
+        verbose=True,
+        allow_dangerous_code=True
+    )
+
+    csv_agent.invoke(
+        input={"input": "how many columns are there in file episode_info.csv"}
+    )
+    csv_agent.invoke(
+        input={
+            "input": "which season has most episodes?"
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
